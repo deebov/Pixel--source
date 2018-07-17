c99f294c21c0e2f2910df1726cc68ab7be42559d
Dilshod Turobov <deebov@yandex.com>
2018-07-17 13:01:20 -0700

    watch all *.js files

--- gulp/tasks/watcher.js
+++ gulp/tasks/watcher.js
@@ -1,6 +1,7 @@
 module.exports = function () {
   $.gulp.task('watcher', function () {
     $.gulp.watch(`./${$.config.srcDir}/pug/**/*.pug`, $.gulp.series('pug', 'inject'));
+    $.gulp.watch(`./${$.config.srcDir}/js/**/*.js`, $.gulp.series('js', 'inject'));
     $.gulp.watch(`./${$.config.srcDir}/style/**/*.sass`, $.gulp.series('sass'));
     $.gulp.watch(`./${$.config.root}`).on('change', $.browserSync.reload);
   });
